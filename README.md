3dede81f2cafa479737aa5c78978289b4a546520
krissemicolon <kris@krishuber.xyz>
2022-10-11 10:59:44 +0200

    readme & cpal init

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # hacksynth
+> Synthesizer Project of Lian Studer & Kris Huber for `[yth22]`
 
-
+![Neubad](assets/neubad.jpg)
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,21 @@ pub enum Message {
 }
 
 pub fn main() {
+    let host = cpal::default_host();
+
+    let device = host
+        .default_output_device()
+        .expect("failed to find a default output device");
+    let config = device.default_output_config().unwrap();
+
+    /*
+    match config.sample_format() {
+        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()).unwrap(),
+        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()).unwrap(),
+        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()).unwrap(),
+    }
+     */
+
     App::run(Settings::default()).unwrap();
 }
 
