879c48bfa4c08edf5c1bae95282489d5543645b9
Kris Huber <kris@krishuber.xyz>
2022-10-28 15:27:18 +0200

    Oscillator Struct

--- src/audio.rs
+++ src/audio.rs
@@ -1,14 +1,33 @@
+use fundsp::{hacker::sine_hz, prelude::*};
 use midir::MidiInputPort;
 
 use crate::device;
 
 pub struct Audio {
     midi: Option<MidiInputPort>,
+    oscillators: Vec<Oscillator>,
 }
 
 impl Audio {
     pub fn new() -> Audio {
         let midi = device::get_midi_device();
-        Audio { midi }
+        Audio {
+            midi,
+            oscillators: vec![Oscillator::new()],
+        }
+    }
+}
+
+pub struct Oscillator {
+    detune: f64,
+}
+
+impl Oscillator {
+    pub fn new() -> Oscillator {
+        Oscillator { detune: 0.0 }
+    }
+
+    pub fn sound(freq: f64) -> An<Pipe<f64, Constant<U1, f64>, Sine<f64>>> {
+        sine_hz(freq)
     }
 }
