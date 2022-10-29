295350485d49f6a1e022829ec29e055776736f59
Kris Huber <kris@krishuber.xyz>
2022-10-29 03:36:22 +0200

    osc1 gui

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,19 @@
-use iced::{image, Alignment, Column, Container, Element, Image, Length, Row, Sandbox};
+use iced::{image, Alignment, Column, Container, Element, Image, Length, Row, Sandbox, Text};
 use iced_audio::native::text_marks;
 use iced_audio::text_marks::Group;
 use iced_audio::{
     knob, tick_marks, v_slider, FloatRange, FreqRange, Knob, LogDBRange, Normal, VSlider,
 };
+use midi_msg::MidiMsg;
+use midir::MidiInputConnection;
+use std::sync::Arc;
+use crossbeam_queue::SegQueue;
 
 use crate::audio::Audio;
-use crate::styling;
+use crate::{styling, midi};
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    VSliderDB(Normal),
     Float(Normal),
 }
 
@@ -18,16 +21,9 @@ pub struct App {
     audio: Audio,
 
     detune_range: FloatRange,
-    detune_text_marks: Group,
 
-    detune_knob_state: knob::State,
-
-    fader_range: LogDBRange,
-    fader_state: v_slider::State,
-    fader_state2: v_slider::State,
-    fader_state3: v_slider::State,
-
-    center_tick_mark: tick_marks::Group,
+    osc1_detune: knob::State,
+    osc1_detune_label: String,
 }
 
 impl Sandbox for App {
@@ -38,63 +34,45 @@ impl Sandbox for App {
     }
 
     fn new() -> App {
-        let fader_range = LogDBRange::new(-12.0, 12.0, 0.5.into());
         let detune_range = FloatRange::new(-100.0, 100.0);
 
         App {
             audio: Audio::new(),
 
             detune_range,
-            detune_text_marks: Group::min_max_and_center("-100Hz", "100Hz", "Detune"),
 
-            // osc1
-            detune_knob_state: knob::State::new(detune_range.default_normal_param()),
-
-            // demo elements
-            fader_range,
-            fader_state: v_slider::State::new(fader_range.default_normal_param()),
-            fader_state2: v_slider::State::new(fader_range.default_normal_param()),
-            fader_state3: v_slider::State::new(fader_range.default_normal_param()),
-
-            center_tick_mark: tick_marks::Group::center(tick_marks::Tier::Two),
+            // osc1 state
+            osc1_detune: knob::State::new(detune_range.default_normal_param()),
+            osc1_detune_label: "Detune\n0 Hz".to_string(),
         }
     }
 
     fn update(&mut self, event: Message) {
         match event {
-            Message::VSliderDB(normal) => {
-                let value = self.fader_range.unmap_to_value(normal);
-                println!("Hacksynth Value: {value}");
-            }
             Message::Float(normal) => {
                 let value = self.detune_range.unmap_to_value(normal);
+                self.osc1_detune_label = format!("Detune\n{:+.1}", value);
                 println!("detune osc1: {value} Hz")
             }
         }
     }
 
     fn view(&mut self) -> Element<Message> {
-        // let fader_widget = VSlider::new(&mut self.fader_state, Message::VSliderDB)
-        //     .tick_marks(&self.center_tick_mark);
-        let fader_widget2 = VSlider::new(&mut self.fader_state2, Message::VSliderDB)
-            .tick_marks(&self.center_tick_mark);
-        let fader_widget3 = VSlider::new(&mut self.fader_state3, Message::VSliderDB)
-            .tick_marks(&self.center_tick_mark);
-
         let detune_knob = Knob::new(
-            &mut self.detune_knob_state,
+            &mut self.osc1_detune,
             Message::Float,
             || None,
             || None,
-        )
-        .text_marks(&self.detune_text_marks);
+        );
 
         let osc1 = Container::new(
             Column::new()
                 .align_items(Alignment::Center)
+                .push(Text::new("osc1").size(12))
+                .push(Text::new(&self.osc1_detune_label).size(12))
                 .push(detune_knob)
                 .spacing(5)
-                .padding(40),
+                .padding(5),
         )
         .style(styling::OscillatorContainer)
         .width(Length::Fill);
@@ -133,7 +111,7 @@ impl Sandbox for App {
                         .spacing(20)
                         .padding(20)
                         .align_items(Alignment::Start)
-                        .push(fader_widget2),
+                        // .push(fader_widget2),
                 ),
         ))
         .align_x(iced::alignment::Horizontal::Left)
@@ -155,7 +133,7 @@ impl Sandbox for App {
                         .spacing(20)
                         .padding(20)
                         .align_items(Alignment::Start)
-                        .push(fader_widget3),
+                        // .push(fader_widget3),
                 ),
         ))
         .align_x(iced::alignment::Horizontal::Left)
